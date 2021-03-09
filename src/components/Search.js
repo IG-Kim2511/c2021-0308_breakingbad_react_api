aa9b7f8f950799375012737a6eecc3dac9683071
iikim <iikim2511@gmail.com>
2021-03-09 17:50:48 -0500

    wip

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const Search = () => {
+
+    const [Text, setText] = useState('')
+
     return (
         <section className="search">
 
             <form>
-                <input type="text" className="form-control" placeholder="search characters" autoFocus></input>
+                <input type="text" className="form-control" placeholder="search characters" value={Text} onChange={(e) => setText(e.target.value)}
+                ></input>
             </form>
             
         </section>
